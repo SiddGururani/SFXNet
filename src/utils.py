1b76dcd1f203ad3e89ebd41255de855c65a893c0
SiddGururani <siddfisher@gmail.com>
2018-02-02 13:23:49 -0500

    minor bug fixes

--- src/utils.py
+++ src/utils.py
@@ -17,9 +17,12 @@ def get_chunks(dataset, effect, num_seconds, sr, context):
 	x = frame(x, sr, sr).T
 	y = frame(y, sr, sr).T
 	# Sample num_seconds number of segments
-	ind = np.random.choice(np.arange(x.shape[0]), num_seconds, replace=False)
-	x = x[ind]
-	y = y[ind]
+	if num_seconds < x.shape[0]:
+		ind = np.random.choice(np.arange(x.shape[0]), num_seconds, replace=False)
+		x = x[ind]
+		y = y[ind]
+	else:
+		print('Number of seconds: {} is larger than dataset size: {}'.format(num_seconds, x.shape[0]))
 	# Segment further into context frames
 	x = np.apply_along_axis(frame, 1, x, frame_length=context, hop_length=1)
 	x = np.transpose(x, (0,2,1))
@@ -28,6 +31,19 @@ def get_chunks(dataset, effect, num_seconds, sr, context):
 	y = y.reshape(-1, 1)
 	return {'x': x, 'y': y}
 
-def get_run_name(effect, bidirectional, rnn_type, rnn_layers, rnn_size, lr, context):
+def get_run_name(effect, num_sec_train, bidirectional, rnn_type, rnn_layers, rnn_size, lr, context):
 	b = 'bi' if bidirectional else ''
-	return 'SFXNet_'+effect+'_'+b+rnn_type+'_'+str(rnn_layers)+'_'+str(rnn_size)+'lr'+str(lr)+str(context)
+	return 'SFXNet_'+effect+'_'+str(num_sec_train)+'_'+b+rnn_type+'_'+str(rnn_layers)+'_'+str(rnn_size)+'lr'+str(lr)+'_'+str(context)
+
+def get_params_from_run_name(run_name):
+	params = run_name.split('_')
+	rnn_type = params[3]
+	rnn_layers = int(params[4])
+	rnn_size = int(params[5].split('lr')[0])
+	if 'bi' in rnn_type:
+		bidirectional = True
+		rnn_type = rnn_type.strip('bi')
+	else:
+		bidirectional = False
+	return rnn_type, rnn_layers, rnn_size, bidirectional
+	
